0a3d5435209aaf892c78bec3e9b6a837e21ca76a
Atsushi Takayasu <a.takayasu@gmail.com>
2016-11-12 08:15:05 +0900

    Spring boot Starter をアップロード

---
+++ src/main/java/org/takastudy/tech/sample/FileuploadawsApplication.java
@@ -0,0 +1,12 @@
+package org.takastudy.tech.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileuploadawsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileuploadawsApplication.class, args);
+	}
+}
